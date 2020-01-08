1bbafb5a60a60798d14f965e56970347300767ae
Peter Harrison <pmc.harrison@gmail.com>
2020-01-08 19:24:50 +0000

    Further work

--- dlgr_utils/questions.py
+++ dlgr_utils/questions.py
@@ -1,15 +1,20 @@
 import pandas
 import importlib_resources   
+import json
+
+from operator import attrgetter
 from flask import render_template_string
 
+from dallinger.models import Participant
+
 from dlgr_utils.misc import get_template
-from dlgr_utils import questionnaires
+from dlgr_utils import definitions
 
 class Question():
     def __init__(self):
         "Do nothing"
 
-class InfoQuestion():
+class InfoQuestion(Question):
     def __init__(self, info):
         self.type = "info"
         self.info = info
@@ -22,13 +27,16 @@ class DropdownQuestion(Question):
         self.values = values
         self.labels = labels
 
-def render_questions(questions):
+def render_questions(questions, name = "final", next_page = ""):
     assert all([isinstance(x, Question) for x in questions])
+    assert isinstance(name, str)
+    assert isinstance(next_page, str)
+
     html = get_template("questions.html")
-    render_template_string(html, questions = questions)
+    return render_template_string(html, questions = questions, name = name, next_page = next_page)
 
 def get_gold_msi():
-    file = importlib_resources.open_text(questionnaires, "gold-msi.csv")
+    file = importlib_resources.open_text(definitions, "gold-msi.csv")
     all = pandas.read_csv(file).query("factor_label == 'Musical Training'")
     choice_ids = [str(1 + x) for x in range(7)]
     info = InfoQuestion("Please select the options that best describe your musical background.")
@@ -73,3 +81,35 @@ paid_to_do_music = DropdownQuestion(
     values = ["yes", "no"],
     labels = ["Yes", "No"]
 )
+
+def first(x):
+    return x[0]
+
+def last(x):
+    return x[len(x) - 1]
+
+def get_questionnaire_response(participant_id, questionnaire, field = None, which = "first", must_work = True):
+    assert isinstance(questionnaire, str)
+    assert field is None or isinstance(field, str)
+    assert isinstance(must_work, bool)
+    assert which in ["first", "last"]
+
+    participant = Participant.query.get(participant_id)
+    all_submissions = participant.questions()
+    relevant_submissions = [x for x in all_submissions if x.question == questionnaire]
+    relevant_submissions.sort(key = attrgetter("creation_time"))
+
+    if len(relevant_submissions) == 0:
+        if must_work:
+            raise Exception("No questionnaire submissions found with name '{}' for participant_id {}.".format(questionnaire, participant_id))
+        else:
+            return None
+
+    chosen = first(relevant_submissions) if which == "first" else last(relevant_submissions)
+    responses = json.loads(chosen.response)
+
+    if field is None:
+        return responses
+    else:
+        return responses[field]
+   
