1e77263e677cf780815c018ab562f2a6ac5e4e1a
Peter Harrison <pmc.harrison@gmail.com>
2020-02-20 13:48:40 +0100

    Implementing claim_field

--- dlgr_utils/misc.py
+++ dlgr_utils/misc.py
@@ -1,6 +1,39 @@
 import importlib_resources
 from . import templates
 
+from sqlalchemy import Boolean, String, Integer, exc
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.sql.expression import cast
+
 def get_template(name):
     assert isinstance(name, str)
     return importlib_resources.read_text(templates, name)
+
+def claim_field(db_index, type=None):
+    if type is int:
+        return IntField(db_index).function
+    else:
+        raise NotImplementedError
+        
+class Field():
+    def __init__(self, db_index, from_db, to_db, sql_type):
+        assert 1 <= db_index and db_index <= 5    
+        db_field = f"property{db_index}"
+
+        @hybrid_property
+        def function(self):
+            return from_db(getattr(self, db_field))
+        
+        @function.setter
+        def function(self, value):
+            setattr(self, db_field, to_db(value))
+
+        @function.expression
+        def function(self):
+            return cast(getattr(self, db_field), sql_type)
+
+        self.function = function
+
+class IntField(Field):
+    def __init__(self, db_index):
+        super().__init__(db_index, int, int, Integer)
--- experiment.py
+++ experiment.py
@@ -6,8 +6,22 @@ from dallinger.models import Network, Info, Transformation, Node
 from dallinger.networks import Chain
 from dallinger.nodes import Source
 from dallinger import db, recruiters
+from dallinger.models import Participant
+
+from dallinger.experiment_server.utils import (
+    success_response
+)
+
+import rpdb
 
 import dlgr_utils.monitor
+from dlgr_utils.misc import claim_field
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__file__)
+
+Participant.test = claim_field(1, type=int)
 
 class Exp(dlgr_utils.monitor.Experiment):
     # You can customise these parameters ####
@@ -72,3 +86,15 @@ extra_routes = Blueprint(
 @extra_routes.route("/monitor", methods=["GET"])
 def get_monitor():
     return Exp(db.session).render_monitor_template()
+
+@extra_routes.route("/init-participant/<int:participant_id>", methods=["POST"])
+def init_participant(participant_id):
+    exp = Exp(db.session)
+
+    logger.info("Initialising participant {}...".format(participant_id))
+
+    participant = Participant.query.get(participant_id)
+    participant.test = 3
+
+    exp.save()
+    return success_response()
