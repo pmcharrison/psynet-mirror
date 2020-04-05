13c91ebf0a3c6b7b87a1ae8daefa2584be766277
Peter Harrison <pmc.harrison@gmail.com>
2020-04-05 10:24:00 +0100

    Creating a new 'page' module

--- dlgr_utils/timeline.py
+++ dlgr_utils/timeline.py
@@ -5,8 +5,6 @@ import flask
 import gevent
 import time
 
-from flask import Markup
-
 from typing import List, Optional, Dict, Union, Callable
 
 from .utils import dict_to_js_vars, call_function, check_function_args
@@ -444,46 +442,6 @@ def reactive_seq(
         )
     )
 
-class InfoPage(Page):
-    """
-    This page displays some content to the user alongside a button
-    with which to advance to the next page.
-
-    Parameters
-    ----------
-
-    content:
-        The content to display to the user. Use :class:`flask.Markup`
-        to display raw HTML.
-
-    time_estimate:
-        Time estimated for the page.
-
-    title:
-        Optional title for the page. Use :class:`flask.Markup`
-        to display raw HTML.
-
-    **kwargs:
-        Further arguments to pass to :class:`dlgr_utils.timeline.Page`.
-    """
-
-    def __init__(
-        self, 
-        content: Union[str, Markup], 
-        time_estimate: Optional[float] = None, 
-        title: Optional[Union[str, Markup]] = None,
-        **kwargs
-    ):
-        super().__init__(
-            time_estimate=time_estimate,
-            template_str=get_template("info-page.html"),
-            template_arg={
-                "content": "" if content is None else content,
-                "title": "" if title is None else title
-            },
-            **kwargs
-        )
-
 class EndPage(Page):
     def __init__(self, content="default", title=None):
         if content=="default":
@@ -521,304 +479,12 @@ class EndPage(Page):
         """
         pass
 
-class SuccessfulEndPage(EndPage):
-    """
-    Indicates a successful end to the experiment.
-    """
-    def finalise_participant(self, experiment, participant):
-        participant.complete = True
-
-class UnsuccessfulEndPage(EndPage):
-    """
-    Indicates an unsuccessful end to the experiment.
-    """
-    def __init__(self, content="default", title=None, failure_tags: Optional[List] = None):
-        if content=="default":
-            content = (
-                "Unfortunately you did not meet the criteria to continue in the experiment. "
-                "You will still be paid for the time you spent already. "
-                "Thank you for taking part!"
-            )
-        super().__init__(content=content, title=title)
-        self.failure_tags = failure_tags
-
-    def finalise_participant(self, experiment, participant):
-        if self.failure_tags:
-            assert isinstance(self.failure_tags, list)
-            participant.append_failure_tags(*self.failure_tags)
-        experiment.fail_participant(participant)
-
-
 class Button():
     def __init__(self, button_id, label, min_width, start_disabled=False):
         self.id = button_id
         self.label = label
         self.min_width = min_width
         self.start_disabled = start_disabled
-
-class ResponsePage(Page):
-    def format_answer(self, answer, metadata, experiment, participant):
-        return answer
-
-    def compile_details(self, response, answer, metadata, experiment, participant):
-        """Should provide a dict of supplementary information about the page and (optionally) its response."""
-        raise NotImplementedError
-
-    def process_response(self, response, metadata, experiment, participant):
-        answer = self.format_answer(response["answer"], metadata, experiment, participant)
-        resp = Response(
-            participant=participant,
-            question_label=self.label, 
-            answer=answer,
-            page_type=type(self).__name__,
-            time_taken=metadata["time_taken"],
-            details=self.compile_details(response, answer, metadata, experiment, participant)
-        )
-        participant.answer = resp.answer
-        experiment.session.add(resp)
-        experiment.save()
-        return resp
-
-class NAFCPage(ResponsePage):
-    """
-    This page solicits a multiple-choice response from the participant.
-    By default this response is saved in the database as a
-    :class:`dlgr_utils.timeline.Response` object, 
-    which can be found in the ``Questions`` table. 
-
-    Parameters
-    ----------
-
-    label:
-        Internal label for the page (used to store results).
-
-    prompt:
-        Prompt to display to the user. Use :class:`flask.Markup`
-        to display raw HTML.
-
-    choices:
-        The different options the participant has to choose from.
-
-    time_estimate:
-        Time estimated for the page.
-
-    labels:
-        An optional list of textual labels to apply to the buttons, 
-        which the participant will see instead of ``choices``.
-
-    arrange_vertically:
-        Whether to arrange the buttons vertically.
-
-    min_width:
-        CSS ``min_width`` parameter for the buttons.            
-    """
-
-    def __init__(
-        self,
-        label: str,
-        prompt: Union[str, Markup],
-        choices: List[str],        
-        time_estimate: Optional[float] = None,
-        labels: Optional[List[str]] = None,
-        arrange_vertically: bool = False,
-        min_width: str ="100px"
-    ):
-        self.prompt = prompt
-        self.choices = choices 
-        self.labels = choices if labels is None else labels
-        
-        assert isinstance(self.labels, List)
-        assert len(self.choices) == len(self.labels)
-
-        if arrange_vertically:
-            raise NotImplementedError
-
-        buttons = [
-            Button(button_id=choice, label=label, min_width=min_width)
-            for choice, label in zip(self.choices, self.labels)
-        ]
-        super().__init__(
-            time_estimate=time_estimate,
-            template_str=get_template("nafc-page.html"),
-            label=label,
-            template_arg={
-                "prompt": prompt,
-                "buttons": buttons
-            }
-        )
-
-    def compile_details(self, response, answer, metadata, experiment, participant):
-        # pylint: disable=unused-argument
-        return {
-            "prompt": self.prompt,
-            "choices": self.choices,
-            "labels": self.labels
-        }
-
-class TextInputPage(ResponsePage):
-    """
-    This page solicits a text response from the user.
-    By default this response is saved in the database as a
-    :class:`dlgr_utils.timeline.Response` object, 
-    which can be found in the ``Questions`` table. 
-
-    Parameters
-    ----------
-
-    label:
-        Internal label for the page (used to store results).
-
-    prompt:
-        Prompt to display to the user. Use :class:`flask.Markup`
-        to display raw HTML.
-
-    time_estimate:
-        Time estimated for the page.
-
-    one_line:
-        Whether the text box should comprise solely one line.
-
-    width:
-        Optional CSS width property for the text box.
-
-    height:
-        Optional CSS height property for the text box.
-    """
-    def __init__(
-        self,
-        label: str,
-        prompt: Union[str, Markup],     
-        time_estimate: Optional[float] = None,
-        one_line: bool = True,
-        width: Optional[str] = None, # e.g. "100px"
-        height: Optional[str] = None
-    ):
-        
-        self.prompt = prompt
-
-        if one_line and height is not None:
-            raise ValueError("If <one_line> is True, then <height> must be None.")
-
-        style = (
-            "" if width is None else f"width:{width}"
-            " "
-            "" if height is None else f"height:{height}"
-        )
-
-        super().__init__(
-            time_estimate=time_estimate,
-            template_str=get_template("text-input-page.html"),
-            label=label,
-            template_arg={
-                "prompt": prompt,
-                "one_line": one_line,
-                "style": style
-            }
-        )
-
-    def compile_details(self, response, answer, metadata, experiment, participant):
-        # pylint: disable=unused-argument
-        return {
-            "prompt": self.prompt
-        }
-
-class SliderInputPage(ResponsePage):
-    """
-    This page solicits a slider response from the user.
-    By default this response is saved in the database as a
-    :class:`dlgr_utils.timeline.Response` object,
-    which can be found in the ``Questions`` table.
-
-    Parameters
-    ----------
-
-    label:
-        Internal label for the page (used to store results).
-
-    prompt:
-        Prompt to display to the user. Use :class:`flask.Markup`
-        to display raw HTML.
-
-    time_allotted:
-        Time allotted for the page.
-
-    min:
-        Minimal value of the slider.
-
-    max:
-        Maximum value of the slider.
-
-    step_size:
-        The size of each step in the slider.
-
-    width:
-        Optional CSS width property for the text box.
-
-    height:
-        Optional CSS height property for the text box.
-    """
-    def __init__(
-        self,
-        label: str,
-        prompt: Union[str, Markup],
-        time_allotted: Optional[float] = None,
-        min: Optional[int] = 0,
-        max: Optional[int] = 100,
-        step_size: Optional[int] = 1,
-        width: Optional[str] = None, # e.g. "100px"
-        height: Optional[str] = None
-    ):
-
-        if max <= min:
-            raise ValueError("<max> must be larger than <min>")
-
-        if (max - min) <= step_size*2:
-            raise ValueError("For the given <min> and <max> values the <step_size> needs to be appropriate, i.e. allow at least 2 steps on the slider.")
-
-        self.prompt = prompt
-
-        style = (
-            "" if width is None else f"width:{width}"
-            " "
-            "" if height is None else f"height:{height}"
-        )
-
-        super().__init__(
-            time_allotted=time_allotted,
-            template_str=get_template("slider-input-page.html"),
-            label=label,
-            template_arg={
-                "prompt": prompt,
-                "step_size": step_size,
-                "min": min,
-                "max": max,
-                "style": style
-            }
-        )
-    # TODO don't know what this is; does it present the question?
-    def compile_details(self, response, answer, metadata, experiment, participant):
-        # pylint: disable=unused-argument
-        return {
-            "prompt": self.prompt
-        }
-
-class NumberInputPage(TextInputPage):
-    """
-    This page is like :class:`dlgr_utils.timeline.TextInputPage`,
-    except it forces the user to input a number.
-    See :class:`dlgr_utils.timeline.TextInputPage` for argument documentation.
-    """
-
-    def format_answer(self, answer, metadata, experiment, participant):
-        try:
-            return float(answer)
-        except ValueError:
-            return "INVALID_RESPONSE"
-
-    def validate(self, parsed_response, experiment, participant):
-        if parsed_response.answer == "INVALID_RESPONSE":
-            return FailedValidation("Please enter a number.")
-        return None
 
 class Timeline():
     def __init__(self, *args):
@@ -1016,6 +682,29 @@ def estimate_time_credit(events):
         for event in events
         if event.returns_time_credit
     ])
+    
+class ResponsePage(Page):
+    def format_answer(self, answer, metadata, experiment, participant):
+        return answer
+
+    def compile_details(self, response, answer, metadata, experiment, participant):
+        """Should provide a dict of supplementary information about the page and (optionally) its response."""
+        raise NotImplementedError
+
+    def process_response(self, response, metadata, experiment, participant):
+        answer = self.format_answer(response["answer"], metadata, experiment, participant)
+        resp = Response(
+            participant=participant,
+            question_label=self.label, 
+            answer=answer,
+            page_type=type(self).__name__,
+            time_taken=metadata["time_taken"],
+            details=self.compile_details(response, answer, metadata, experiment, participant)
+        )
+        participant.answer = resp.answer
+        experiment.session.add(resp)
+        experiment.save()
+        return resp
         
 class FailedValidation:
     def __init__(self, message="Invalid response, please try again."):
@@ -1420,7 +1109,7 @@ class BackgroundTask(NullEvent):
             end_time = time.monotonic()
             time_taken = end_time - start_time
             logger.info("The background task '%s' completed in %s seconds.", self.label, f"{time_taken:.3f}")
-        except Exception:
+        except RuntimeError:
             logger.info("An exception was thrown in the background task '%s'.", self.label, exc_info=True)
 
     def daemon(self):
@@ -1442,9 +1131,3 @@ class RecruitmentCriterion(NullEvent):
         self.label = label
         self.function = function
 
-# class RegisterBackgroundTasks(NullEvent):
-#     def __init__(self, tasks):
-#         assert isinstance(tasks, list)
-#         for task in tasks:
-#             assert isinstance(task, BackgroundTask)
-#         self.tasks = tasks
