1de0a664a310f8fb7f2d5eae9ee2f7b33b344581
Peter Harrison <pmc.harrison@gmail.com>
2020-04-07 22:12:07 +0100

    Fixed __init__.py

--- psynet/__init__.py
+++ psynet/__init__.py
@@ -1,4 +1,6 @@
 import os
 
-with open(os.path.join("psynet", 'VERSION')) as version_file:
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'VERSION')) as version_file:
     __version__ = version_file.read().strip()
