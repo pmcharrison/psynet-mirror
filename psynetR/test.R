6d51453356be543f373e468a54a9e4dff175b852
Peter Harrison <pmc.harrison@gmail.com>
2020-05-21 10:24:46 +0100

    Working on R scripts

--- psynetR/test.R
+++
@@ -1,8 +0,0 @@
-path <- "/Users/peter.harrison/Dropbox/Academic/projects/jacoby-nori/cap/ganspace-gsp"
-
-x <- import_gsp(
-  path,
-  label = "gansexp1",
-  node_type = "custom_node",
-  trial_type = "custom_trial"
-)
