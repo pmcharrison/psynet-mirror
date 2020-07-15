f7d9ca78b378afc6fd307befb905abf905e9d44d
Peter Harrison <pmc.harrison@gmail.com>
2020-07-15 13:04:14 +0100

    Trying new CI config

--- setup.py
+++ setup.py
@@ -24,7 +24,23 @@ setuptools.setup(
     ],
     python_requires='>=3.7.0',
     include_package_data=True,
-    install_requires=["dallinger", "click", "datetime", "flask", "importlib_resources", "pandas", "rpdb", "progress", "scipy", "statsmodels"],
+    install_requires=[
+        "dallinger",
+        "click",
+        "datetime",
+        "flask",
+        "importlib_resources",
+        "pandas",
+        "rpdb",
+        "progress",
+        "scipy",
+        "statsmodels"
+    ],
+    extras_require={
+        "dev": [
+            "pytest"
+        ]
+    },
     entry_points={
         "console_scripts": [
             "psynet = psynet.command_line:psynet"
