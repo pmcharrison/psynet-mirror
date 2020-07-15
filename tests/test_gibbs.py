26e8c02d23c95b0a766b6ab6f8f1de7599d047ec
Peter Harrison <pmc.harrison@gmail.com>
2020-07-15 15:32:51 +0100

    Trying to get tests working

--- tests/test_gibbs.py
+++ tests/test_gibbs.py
@@ -4,8 +4,13 @@ from statistics import mean
 
 from dallinger.models import Network
 from dallinger.experiment import Experiment
+from dallinger.pytest_dallinger import db_session
 
-def make_gibbs_node(cls):
+@pytest.fixture
+def experiment(db_session):
+    return Experiment(session=db_session)
+
+def make_gibbs_node(cls, experiment):
     seed = {
         "vector": [0, 1],
         "active_index": 0
@@ -14,15 +19,15 @@ def make_gibbs_node(cls):
         seed=seed,
         degree=1,
         network=Network(),
-        experiment=Experiment(),
+        experiment=experiment,
         propagate_failure=False,
         participant=None
     )
 
-def test_summarise():
+def test_summarise(experiment):
     class Node1(GibbsNode):
         summarise_trials_method = "mean"
-    n1 = make_gibbs_node(Node1)
+    n1 = make_gibbs_node(Node1, experiment)
 
     observations = [0, 1, 8, 9, 10]
 
@@ -30,19 +35,19 @@ def test_summarise():
 
     class Node2(GibbsNode):
         summarise_trials_method = "median"
-    n2 = make_gibbs_node(Node2)
+    n2 = make_gibbs_node(Node2, experiment)
 
     assert n2.summarise_trial_dimension(observations) == 8
 
     class Node3(GibbsNode):
         summarise_trials_method = "kernel_mode"
         kernel_width = [1]
-    n3 = make_gibbs_node(Node3)
+    n3 = make_gibbs_node(Node3, experiment)
 
     class Node4(GibbsNode):
         summarise_trials_method = "kernel_mode"
         kernel_width = [7]
-    n4 = make_gibbs_node(Node4)
+    n4 = make_gibbs_node(Node4, experiment)
 
     assert n3.summarise_trial_dimension(observations) == 9.0
     assert 5.9 < n4.summarise_trial_dimension(observations) < 6.1
@@ -54,12 +59,12 @@ def test_summarise():
     class Node5a(GibbsNode):
         summarise_trials_method = "kernel_mode"
         kernel_width = "cv_ls"
-    n5a = make_gibbs_node(Node5a)
+    n5a = make_gibbs_node(Node5a, experiment)
 
     class Node5b(GibbsNode):
         summarise_trials_method = "kernel_mode"
         # kernel_width should be the same as Node4a, because cv_ls is the default
-    n5b = make_gibbs_node(Node5b)
+    n5b = make_gibbs_node(Node5b, experiment)
 
     observations_3 = [0, 2, 3]
 
