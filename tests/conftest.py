15f02fdae28416f038d4a3f757542148cec5835e
Peter Harrison <pmc.harrison@gmail.com>
2020-07-16 14:03:43 +0100

    Attempting to run Chrome tests remotely

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,22 @@ def pytest_addoption(parser):
                      action='store_true',
                      help='enable headless mode for supported browsers.')
 
-@pytest.fixture
-def chrome_options(chrome_options, pytestconfig):
-    chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    if pytestconfig.getoption('headless'):
-        chrome_options.add_argument('--headless')
-    return chrome_options
+# @pytest.fixture
+# def chrome_options(chrome_options, pytestconfig):
+#     chrome_options.add_argument("--remote-debugging-port=9222")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
+#     chrome_options.add_argument("--no-sandbox")
+#     if pytestconfig.getoption('headless'):
+#         chrome_options.add_argument('--headless')
+#     return chrome_options
+
+# from selenium.webdriver.chrome.options import Options
+#
+# chrome_options = Options()
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument("--remote-debugging-port=9222")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
 
 @pytest.fixture
 def firefox_options(firefox_options, pytestconfig):
--- tests/test_demo_timeline.py
+++ tests/test_demo_timeline.py
@@ -2,6 +2,8 @@ import os
 import pytest
 import re
 import logging
+
+from cached_property import cached_property
 from selenium.common.exceptions import TimeoutException
 
 logger = logging.getLogger(__file__)
@@ -19,6 +21,19 @@ class PYTEST_BOT_CLASS(BotBase):
         except TimeoutException:
             logger.error("Error during experiment sign off.")
             return False
+
+    @cached_property
+    def driver(self):
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+
+        return webdriver.Chrome()
 
 import time
 
