6bf513ac3a3190a2814c4025bf296309a70a7c54
Peter Harrison <pmc.harrison@gmail.com>
2020-07-16 13:44:09 +0100

    Attempting to run Chrome tests emotely

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,8 @@ def pytest_addoption(parser):
 @pytest.fixture
 def chrome_options(chrome_options, pytestconfig):
     chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     if pytestconfig.getoption('headless'):
         chrome_options.add_argument('--headless')
     return chrome_options
