bbab638e696d5c8df36c9f213c9bc4b15f0a0170
Peter Harrison <pmc.harrison@gmail.com>
2020-11-11 19:56:29 +0000

    Fix estimated_max_bonus and estimated_completion_time

--- psynet/experiment.py
+++ psynet/experiment.py
@@ -142,12 +142,12 @@ class Experiment(dallinger.experiment.Experiment):
         return sum([(0.0 if p.base_payment is None else p.base_payment) + (0.0 if p.bonus is None else p.bonus) for p in Participant.query.all()])
 
     @classmethod
-    def estimated_max_bonus(cls, wage_per_hour):
-        return cls.timeline.estimated_max_bonus(wage_per_hour)
+    def estimated_max_bonus(cls):
+        return cls.timeline.estimated_max_bonus()
 
     @classmethod
-    def estimated_completion_time(cls, wage_per_hour):
-        return cls.timeline.estimated_completion_time(wage_per_hour)
+    def estimated_completion_time(cls):
+        return cls.timeline.estimated_completion_time()
 
     @property
     def experiment_initialized(self):
