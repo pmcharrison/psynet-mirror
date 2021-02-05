dd8d1cc8aabb01cb6d8a8e064f99237fff9e46ee
Frank Höger <frank.hoeger@ae.mpg.de>
2021-02-05 14:02:10 +0100

    Cleanup

--- demos/unity/static/scripts/experiment.js
+++
@@ -1,9 +0,0 @@
-function assert(condition, message) {
-  if (!condition) {
-    message = message || "Assertion failed";
-    if (typeof Error !== "undefined") {
-      throw new Error(message);
-    }
-    throw message;
-  }
-}
