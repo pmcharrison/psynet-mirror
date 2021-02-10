d318fcb0f54e36cd26b9918093a4c9fabc6ca63e
Manuel Anglada-Tort <manel.anglada.tort@gmail.com>
2021-02-10 16:34:36 +0000

    Resolve CAP-570 "Manu prescreens"

---
+++ demos/attention_check/experiment.py
@@ -0,0 +1,40 @@
+##########################################################################################
+#### Imports
+##########################################################################################
+
+import psynet.experiment
+from psynet.page import InfoPage, SuccessfulEndPage
+from psynet.prescreen import AttentionCheck
+from psynet.timeline import Timeline
+
+from psynet.demography.general import (
+    BasicDemography,
+    Language,
+    BasicMusic,
+    HearingLoss,
+    Dance
+)
+
+##########################################################################################
+#### Experiment
+##########################################################################################
+
+# Weird bug: if you instead import Experiment from psynet.experiment,
+# Dallinger won't allow you to override the bonus method
+# (or at least you can override it but it won't work).
+class Exp(psynet.experiment.Experiment):
+    consent_audiovisual_recordings = False
+
+    timeline = Timeline(
+        BasicDemography(),
+        Language(),
+        BasicMusic(),
+        # It is a good practice to add the AttentionCheck in the middle of demographic questions, so its presence is
+        # not too obvious
+        AttentionCheck(),
+        HearingLoss(),
+        Dance(),
+        SuccessfulEndPage()
+    )
+
+extra_routes = Exp().extra_routes()
