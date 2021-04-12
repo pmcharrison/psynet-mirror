fd863c93eed2f8e3ef8c5fdd9c7040f30aa58a23
Frank Höger <frank.hoeger@ae.mpg.de>
2021-04-12 02:14:13 +0200

    Update unity demo

--- demos/unity/experiment.py
+++ demos/unity/experiment.py
@@ -1,9 +1,10 @@
-import json
+# pylint: disable=unused-import,abstract-method
+
 import logging
 import random
+from typing import List, Optional
 
 import psynet.experiment
-from psynet.modular_page import ModularPage, PushButtonControl
 from psynet.page import SuccessfulEndPage, UnityPage
 from psynet.timeline import CodeBlock, Timeline
 from psynet.trial.non_adaptive import (
@@ -13,28 +14,41 @@ from psynet.trial.non_adaptive import (
     StimulusSpec,
 )
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
 ##########################################################################################
-# Stimuli
+#    Stimuli
 ##########################################################################################
-debug = False
-number_of_islands = 1  # How many islands to visit in each world
-number_of_islands_final = 1  # How many islands to visit in the final world
+DEBUG = False
+NUMBER_OF_ISLANDS = 1  # How many islands to visit in each world
+NUMBER_OF_FINAL_ISLANDS = 2  # How many islands to visit in each world
 
 # Permutations of colors
-permutations = [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]]
+POSSIBLE_WORLD_ORDERS = [
+    [0, 1, 2],
+    [0, 2, 1],
+    [1, 0, 2],
+    [1, 2, 0],
+    [2, 0, 1],
+    [2, 1, 0],
+]
 
-all_colors = ["red", "green", "yellow"]
-all_types = [0, 1, 2]
-all_rates = [0, 50, 100]
+POSSIBLE_COLORS = ["red", "green", "yellow"]
+WORLD_IDS = [0, 1, 2]
+WORLD_FEEDBACK_RATES = [0, 50, 100]
+
+SAME_SESSION_ID = "0"
 
 
 # Definition of network
-stimulus_set = StimulusSet(
+stimulus_set_initial = StimulusSet(
     "islands",
-    [StimulusSpec(definition={"mtype": mtype}, phase="train") for mtype in all_types],
+    [
+        StimulusSpec(definition={"world_id": world_id}, phase="train")
+        for world_id in WORLD_IDS
+    ],
 )
 
 
@@ -42,31 +56,33 @@ stimulus_set_final = StimulusSet(
     "final_islands",
     [
         StimulusSpec(
-            definition={"mtype": mtype},
+            definition={"world_id": world_id},
             phase="experiment",
+            participant_group="{}".format(world_id),
         )
-        for mtype in all_types
+        for world_id in WORLD_IDS
     ],
 )
 
 
 class UnityIslandPage(UnityPage):
-    def __init__(self, contents, session_id):
-        self.title = "Unity Demo – Ferry Game"
-        self.game_container_width = "960px"
-        self.game_container_height = "600px"
-        self.resources = "/static"
-        self.time_estimate = 5
-        self.contents = contents
-        self.session_id = session_id
-        self.debug = debug
+    def __init__(
+        self,
+        contents: dict,
+        session_id: str,
+        time_estimate: Optional[float] = None,
+        **kwargs,
+    ):
 
         super().__init__(
-            title=self.title,
-            resources=self.resources,
-            contents=self.contents,
-            session_id=self.session_id,
-            debug=self.debug,
+            title="Unity - FerryGov",
+            resources="/static",
+            contents=contents,
+            session_id=session_id,
+            debug=DEBUG,
+            time_estimate=time_estimate,
+            game_container_width="960px",
+            game_container_height="600px",
         )
 
     def format_answer(self, raw_answer, **kwargs):
@@ -77,16 +93,41 @@ class UnityIslandPage(UnityPage):
         return raw_answer
 
 
-# A trial has several pages. Each island is a page. All islands are in one trial.
+class UnityQuestionPage(UnityPage):
+    def __init__(
+        self,
+        question: str,
+        options: List[str],
+        session_id: str,
+        time_estimate: Optional[float] = None,
+        **kwargs,
+    ):
+        contents = {"question": question, "options": options}
+        super().__init__(
+            title="Unity - FerryGov: question",
+            resources="/static",
+            contents=contents,
+            session_id=session_id,
+            debug=DEBUG,
+            time_estimate=time_estimate,
+            game_container_width="960px",
+            game_container_height="600px",
+        )
+
+    def format_answer(self, raw_answer, **kwargs):
+        return raw_answer["answer"]
+
+
+# Trial has several pages often input and output. Each island is a page. All islands are in one trial.
 class IslandTrial(NonAdaptiveTrial):
     __mapper_args__ = {"polymorphic_identity": "island_trial"}
-    num_pages = number_of_islands
+    num_pages = NUMBER_OF_ISLANDS
     accumulate_answers = True
 
     # How we build the series of islands
     def show_trial(self, experiment, participant):
         network_content = self.definition
-        mtype = int(network_content["mtype"])
+        world_id = int(network_content["world_id"])
 
         # Map type to a permutation that depends only on particpant number
         my_perm = participant.var.color_permutation
@@ -95,89 +136,53 @@ class IslandTrial(NonAdaptiveTrial):
         participant.var.set("permutation", my_perm)
 
         # Based on type define variables that determine Unity behaviour.
-        dashboard_rate = all_rates[mtype]
-        feedback_rate = all_rates[mtype]
-
-        # Use the participant specific permutation to determine color
-        island_color = my_perm[mtype]
+        dashboard_rate = WORLD_FEEDBACK_RATES[world_id]
+        feedback_rate = WORLD_FEEDBACK_RATES[world_id]
 
         # Prepare data for JSON
         data = {
             "dashboard_rate": dashboard_rate,
             "feedback_rate": feedback_rate,
-            "island_color": island_color,
+            "island_color": self.get_island_color(
+                my_perm=my_perm, world_id=world_id, participant=participant
+            ),
         }
 
-        # Convert dictionary into a string as Unity wants the data as a string
-        data_as_json = json.dumps(data)
         page = UnityIslandPage(
-            # Send this string to Unity
-            contents=data_as_json,
-            # We stay in the same Unity session by using the same session_id across multiple UnityPages.
-            session_id=str(participant.id * 1000),
+            contents=data, session_id=SAME_SESSION_ID, time_estimate=5
         )
+
         list_of_pages = [page] * (self.num_pages)
         return list_of_pages
 
+    def get_island_color(self, **kwargs):
+        return kwargs["my_perm"][kwargs["world_id"]]
 
-class FinalIslandTrial(NonAdaptiveTrial):
+
+class FinalIslandTrial(IslandTrial):
     __mapper_args__ = {"polymorphic_identity": "final_island_trial"}
-    num_pages = number_of_islands_final
-    accumulate_answers = True
+    num_pages = NUMBER_OF_FINAL_ISLANDS
 
-    # How we build the series of islands
-    def show_trial(self, experiment, participant):
-        network_content = self.definition
-        mtype = int(network_content["mtype"])
-
-        # Map type to a permutation that depends only on particpant number
-        my_perm = participant.var.color_permutation
-
-        # Based on type define variables that determine Unity behaviour.
-        dashboard_rate = all_rates[mtype]
-        feedback_rate = all_rates[mtype]
-
-        # Use the participant's choice for the final world
-        island_color = my_perm.index(participant.var.final_world)
-
-        # Prepare data for JSON
-        data = {
-            "dashboard_rate": dashboard_rate,
-            "feedback_rate": feedback_rate,
-            "island_color": island_color,
-        }
-
-        # Convert dictionary into a string as Unity wants the data as a string
-        data_as_json = json.dumps(data)
-        logger.info(f"Final island is: {participant.var.final_world}")
-        page = UnityIslandPage(
-            contents=data_as_json,
-            session_id=str(participant.id * 1000),
-        )
-        list_of_pages = [page] * (self.num_pages)
-        return list_of_pages
+    def get_island_color(self, **kwargs):
+        return kwargs["participant"].get_participant_group("FinalIslands")
 
 
 class IslandTrialMaker(NonAdaptiveTrialMaker):
     response_timeout_sec = 1000
 
     def compute_bonus(self, score, passed):
-        bonus = score / 100
-        logger.info(f"Accumulated score: {bonus}")
-        return bonus
+        return score / 100
 
     def performance_check(self, experiment, participant, participant_trials):
         """
         Should return a tuple (score: float, passed: bool)
         """
-        score = 0
+        score = 1  # should be zero
         for trial in participant_trials:
             for answer in trial.answer:
-                data = json.loads(answer)
+                data = answer  # json.loads(answer)
                 number_of_coins_in_trial = len(data["coins"])
-                score = score + number_of_coins_in_trial
-
-        logger.info(f"Accumulated score: {score}")
+                score += number_of_coins_in_trial
         passed = True
 
         return {"score": score, "passed": passed}
@@ -187,7 +192,7 @@ trial_maker = IslandTrialMaker(
     id_="Islands",
     trial_class=IslandTrial,
     phase="train",
-    stimulus_set=stimulus_set,
+    stimulus_set=stimulus_set_initial,
     time_estimate_per_trial=3,
     max_trials_per_block=3,
     allow_repeated_stimuli=False,
@@ -226,29 +231,36 @@ final_trial_maker = IslandTrialMaker(
 # Experiment
 ##########################################################################################
 
-
 # Weird bug: if you instead import Experiment from psynet.experiment,
 # Dallinger won't allow you to override the bonus method
 # (or at least you can override it but it won't work).
+
+
 class Exp(psynet.experiment.Experiment):
     consent_audiovisual_recordings = False
 
     timeline = Timeline(
         CodeBlock(
             lambda participant: participant.var.set(
-                "color_permutation", random.sample(permutations, 1)[0]
+                "color_permutation", random.sample(POSSIBLE_WORLD_ORDERS, 1)[0]
             )
         ),
         trial_maker,
-        ModularPage(
-            "choose_world",
-            "In which world do you want to play for the rest of the game?",
-            control=PushButtonControl(["0", "1", "2"]),
-            time_estimate=5,
+        # Use this to have question inside Unity:
+        UnityQuestionPage(
+            "Choose world?", ["0", "1", "2"], SAME_SESSION_ID, time_estimate=5
         ),
+        # Uncomment below code to have a Psynet question page:
+        # from psynet.modular_page import ModularPage, Prompt, PushButtonControl
+        # ModularPage(
+        #     "choose_world",
+        #     Prompt("In which world do you want to play for the rest of the game?"),
+        #     control=PushButtonControl(["0", "1", "2"]),
+        #     time_estimate=5,
+        # ),
         CodeBlock(
-            lambda participant: participant.var.set(
-                "final_world", int(participant.answer)
+            lambda participant: participant.set_participant_group(
+                "FinalIslands", participant.answer
             )
         ),
         final_trial_maker,
