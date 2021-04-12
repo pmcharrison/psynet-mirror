54e1f0380935214e3e4f94b3cf1cc576a9248e2b
Frank Höger <frank.hoeger@ae.mpg.de>
2021-04-12 13:20:21 +0000

    CAP-796: Add CapRecruiter

---
+++ psynet/recruiters.py
@@ -0,0 +1,81 @@
+import os
+
+import dallinger.recruiters
+import requests
+
+from .utils import get_logger
+
+logger = get_logger()
+
+
+class BaseCapRecruiter(dallinger.recruiters.CLIRecruiter):
+
+    """
+    The CapRecruiter base class
+
+    """
+
+    def open_recruitment(self, n=1):
+        """
+        Return an empty list which otherwise would be a list of recruitment URLs.
+        """
+        return {"items": [], "message": ""}
+
+    def recruit(self, n=1):
+        return []
+
+    def close_recruitment(self):
+        logger.info("No more participants required. Recruitment stopped.")
+
+    def compensate_worker(self, *args, **kwargs):
+        """A recruiter may provide a means to directly compensate a worker."""
+        raise RuntimeError("Compensation is not implemented.")
+
+    def reward_bonus(self, assignment_id, amount, reason):
+        """
+        Return values for `basePay` and `bonus` to cap-recruiter application.
+        """
+        data = {
+            "assignmentId": assignment_id,
+            "basePayment": self.config.get("base_payment"),
+            "bonus": amount,
+        }
+        requests.post(
+            self.external_submission_url,
+            json=data,
+            headers={"Authorization": os.environ.get("CAP_RECRUITER_AUTH_TOKEN")},
+            verify=False,  # Temporary fix because of SSLCertVerificationError
+        )
+
+
+class CapRecruiter(BaseCapRecruiter):
+
+    """
+    The production cap-recruiter.
+
+    """
+
+    nickname = "cap-recruiter"
+    external_submission_url = "https://cap-recruiter.ae.mpg.de/hits/complete"
+
+
+class StagingCapRecruiter(BaseCapRecruiter):
+
+    """
+    The staging cap-recruiter.
+
+    """
+
+    nickname = "staging-cap-recruiter"
+    external_submission_url = "https://staging-cap-recruiter.ae.mpg.de/hits/complete"
+
+
+class DevCapRecruiter(BaseCapRecruiter):
+
+    """
+    The development cap-recruiter.
+
+    """
+
+    nickname = "dev-cap-recruiter"
+    external_submission_url = "http://localhost:8000/hits/complete"
