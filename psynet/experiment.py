38756c4222c74c2b7c56625c3eeb60a665a06a9d
Peter Harrison <pmc.harrison@gmail.com>
2021-05-11 18:30:43 +0200

    Prevent multiple instances of check_database from running simultaneously

--- psynet/experiment.py
+++ psynet/experiment.py
@@ -118,7 +118,7 @@ class Experiment(dallinger.experiment.Experiment):
         else:
             self.register_pre_deployment_routines()
 
-    @scheduled_task("interval", minutes=1)
+    @scheduled_task("interval", minutes=1, max_instances=1)
     @staticmethod
     def check_database():
         exp_class = dallinger.experiment.load()
