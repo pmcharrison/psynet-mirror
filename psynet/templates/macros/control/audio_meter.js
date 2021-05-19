6f943fb313c450a22a49ff5c9872f3de76428db6
Peter Harrison <pmc.harrison@gmail.com>
2021-05-19 12:23:08 +0200

    Fix audio meter

--- psynet/templates/macros/control/audio_meter.js
+++ psynet/templates/macros/control/audio_meter.js
@@ -53,7 +53,7 @@ audio_meter_control.init = function(json) {
     this.message_timer = null;
 
     var audio_meter_control = this;
-    psynet.trial.onEvent("construct",function() {
+    psynet.trial.onEvent("trialConstruct",function() {
         audio_meter_control.canvasContext = document.getElementById( "audio_meter" ).getContext("2d");
         audio_meter_control.audioContext = psynet.media.audio_context;
         return new Promise((resolve) => {
