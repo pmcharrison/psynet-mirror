0e99383a10bae99ca701f9d9636473ab63fba784
Frank Höger <frank.hoeger@ae.mpg.de>
2021-08-10 15:16:58 +0200

    Bump version to 3.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="psynet",
-    version="3.0.0",
+    version="3.1.0",
     author="Peter Harrison, Raja Marjieh, Nori Jacoby",
     author_email="pmc.harrison@gmail.com",
     description="Utility functions for Dallinger experiments",
