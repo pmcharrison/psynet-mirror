f6dbd55d06eef42675cd06992802c97be130308a
Frank Höger <frank.hoeger@ae.mpg.de>
2021-09-27 15:09:41 +0200

    Update authorization

--- psynet/recruiters.py
+++ psynet/recruiters.py
@@ -1,7 +1,6 @@
 import dallinger.recruiters
 import requests
-
-# from dallinger.config import get_config
+from dallinger.config import get_config
 from dallinger.db import session
 
 from .utils import get_logger
@@ -53,7 +52,7 @@ class BaseCapRecruiter(dallinger.recruiters.CLIRecruiter):
         requests.post(
             self.external_submission_url,
             json=data,
-            headers={"Authorization": "da128d326d10a5fc336af3efcddb7ae133ed5d83"},
+            headers={"Authorization": get_config().get("cap_recruiter_auth_token")},
             verify=False,  # Temporary fix because of SSLCertVerificationError
         )
 
