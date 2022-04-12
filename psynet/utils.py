3f65e2ae235ade9b7806b077ec64ab13ff68d5d9
Peter Harrison <pmch2@cam.ac.uk>
2022-04-12 16:57:40 +0200

    Rename klass -> cls

--- psynet/utils.py
+++ psynet/utils.py
@@ -497,10 +497,10 @@ class ClassPropertyDescriptor(object):
         self.fget = fget
         self.fset = fset
 
-    def __get__(self, obj, klass=None):
-        if klass is None:
-            klass = type(obj)
-        return self.fget.__get__(obj, klass)()
+    def __get__(self, obj, cls=None):
+        if cls is None:
+            cls = type(obj)
+        return self.fget.__get__(obj, cls)()
 
     def __set__(self, obj, value):
         if not self.fset:
