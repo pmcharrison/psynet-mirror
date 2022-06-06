bf872a20fff2870b3d4067e8a566a2e5418920d0
Peter Harrison <pmch2@cam.ac.uk>
2022-06-06 11:43:19 +0200

    Add 'name' to MCMCP demo

--- demos/mcmcp/experiment.py
+++ demos/mcmcp/experiment.py
@@ -80,6 +80,8 @@ class CustomNode(MCMCPNode):
 # Dallinger won't allow you to override the bonus method
 # (or at least you can override it but it won't work).
 class Exp(psynet.experiment.Experiment):
+    name = "MCMCP demo experiment"
+
     variables = {
         "show_abort_button": True,
     }
