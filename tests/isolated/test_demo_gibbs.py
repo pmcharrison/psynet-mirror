4cc8c8dedbf417e630a615e9ce4c09e9552794e6
Peter Harrison <pmch2@cam.ac.uk>
2022-07-10 17:41:37 +0200

    Add test_populate_db_from_zip_file

--- tests/isolated/test_demo_gibbs.py
+++ tests/isolated/test_demo_gibbs.py
@@ -12,7 +12,10 @@ from selenium.webdriver.common.by import By
 
 from psynet.command_line import export_
 from psynet.field import UndefinedVariableError
-from psynet.test import bot_class, next_page
+from psynet.participant import Participant
+from psynet.test import assert_text, bot_class, next_page
+from psynet.timeline import Response
+from psynet.trial.main import Trial
 
 logger = logging.getLogger(__file__)
 PYTEST_BOT_CLASS = bot_class()
@@ -40,8 +43,6 @@ class TestExp:
 
             next_page(driver, "next-button")
 
-            from psynet.participant import Participant
-
             pt = Participant.query.filter_by(id=participant + 1).one()
 
             # This variable is set in a code block within the trial
@@ -56,6 +57,27 @@ class TestExp:
                 pt.var.get("uninitialized_variable")
 
             assert pt.var.get("uninitialized_variable", default=123) == 123
+
+            assert_text(driver, "main-body", "Did you like the experiment? Next")
+            text_input = driver.find_element(By.ID, "text-input")
+            text_input.send_keys("Yes, I loved it!")
+            next_page(driver, "next-button")
+
+            assert_text(
+                driver, "main-body", "Did you find the experiment difficult? Next"
+            )
+            text_input = driver.find_element(By.ID, "text-input")
+            text_input.send_keys("No, I found it easy.")
+            next_page(driver, "next-button")
+
+            assert_text(
+                driver,
+                "main-body",
+                "Did you encounter any technical problems during the experiment? If so, please provide a few words describing the problem. Next",
+            )
+            text_input = driver.find_element(By.ID, "text-input")
+            text_input.send_keys("No technical problems.")
+            next_page(driver, "next-button")
 
             next_page(driver, "next-button", finished=True)
 
@@ -130,6 +152,26 @@ class TestExp:
 
         test_psynet_exports(data_dir)
 
+        def test_experiment_feedback(data_dir):
+            df = pandas.read_csv(os.path.join(data_dir, "Response.csv"))
+
+            df_ = df.query("question == 'liked_experiment'")
+            assert df_.shape[0] == 4
+            assert list(df_.participant_id) == [1, 2, 3, 4]
+            assert list(df_.answer) == ["Yes, I loved it!"] * 4
+
+            df_ = df.query("question == 'find_experiment_difficult'")
+            assert df_.shape[0] == 4
+            assert list(df_.participant_id) == [1, 2, 3, 4]
+            assert list(df_.answer) == ["No, I found it easy."] * 4
+
+            df_ = df.query("question == 'encountered_technical_problems'")
+            assert df_.shape[0] == 4
+            assert list(df_.participant_id) == [1, 2, 3, 4]
+            assert list(df_.answer) == ["No technical problems."] * 4
+
+        test_experiment_feedback(data_dir)
+
         def test_dallinger_exports(zip_file):
             with tempfile.TemporaryDirectory() as tempdir:
                 with zipfile.ZipFile(zip_file, "r") as zip_ref:
@@ -143,5 +185,38 @@ class TestExp:
                     assert dallinger_csv_files == [t + ".csv" for t in db_tables]
 
         test_dallinger_exports(zip_file)
+
+        def test_populate_db_from_zip_file(zip_file):
+            import pydevd_pycharm
+            from dallinger import db
+            from dallinger.db import init_db
+            from sqlalchemy.orm.session import close_all_sessions
+
+            from psynet.command_line import populate_db_from_zip_file
+
+            pydevd_pycharm.settrace(
+                "localhost", port=12345, stdoutToServer=True, stderrToServer=True
+            )
+
+            # Without this, init_db can freeze --
+            # https://stackoverflow.com/questions/24289808/drop-all-freezes-in-flask-with-sqlalchemy
+            db.session.commit()
+            close_all_sessions()
+            init_db(drop_all=True)
+            populate_db_from_zip_file(zip_file)
+
+            trials = Trial.query.all()
+            assert len(trials) > 15
+            assert all(t.participant_id in [1, 2, 3, 4] for t in trials)
+
+            participants = Participant.query.all()
+            assert len(participants) == 4
+            assert sorted([p.id for p in participants]) == [1, 2, 3, 4]
+
+            responses = Response.query.all()
+            assert len(responses) > 15
+            assert sorted([r.id for r in responses]) == [1, 2, 3, 4]
+
+        test_populate_db_from_zip_file(zip_file)
 
         shutil.rmtree("data")
