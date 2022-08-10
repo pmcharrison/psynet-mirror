dfe256aa5682771b7142076ab5f206c4bd941288
Peter Harrison <pmch2@cam.ac.uk>
2022-08-10 21:34:56 +0100

    WIP

--- demos/repp_tests/experiment.py
+++ demos/repp_tests/experiment.py
@@ -1,8 +1,8 @@
 # pylint: disable=unused-import,abstract-method,unused-argument,no-member
 
 import psynet.experiment
+from psynet.asset import DebugStorage, S3Storage  # noqa
 from psynet.consent import NoConsent
-from psynet.media import prepare_s3_bucket_for_presigned_urls
 from psynet.page import InfoPage, SuccessfulEndPage
 from psynet.prescreen import (
     FreeTappingRecordTest,
@@ -11,32 +11,23 @@ from psynet.prescreen import (
     REPPVolumeCalibrationMarkers,
     REPPVolumeCalibrationMusic,
 )
-from psynet.timeline import PreDeployRoutine, Timeline
-
-# TODO - this needs updating
+from psynet.timeline import Timeline
 
 
 # Experiment
 class Exp(psynet.experiment.Experiment):
     label = "REPP tests demo"
 
+    # asset_storage = S3Storage("psynet-demos", "repp-tests")
+    asset_storage = DebugStorage()
+
     timeline = Timeline(
         NoConsent(),
-        PreDeployRoutine(
-            "prepare_s3_bucket_for_presigned_urls",
-            prepare_s3_bucket_for_presigned_urls,
-            {
-                "bucket_name": "markers-check-recordings",
-                "public_read": True,
-                "create_new_bucket": True,
-            },  # s3 bucket to store markers check recordings
-        ),
-        # volume calibration tests
+        # Volume calibration tests
         REPPVolumeCalibrationMarkers(),  # Use this for SMS experiemnts with markers
-        REPPVolumeCalibrationMusic(),  # Use this for expeeriments using music
-        # tappingn instructions and calibration
-        REPPTappingCalibration(),
-        # recording tests
+        REPPVolumeCalibrationMusic(),  # Use this for experiments using music
+        REPPTappingCalibration(),  # Tapping instructions and calibration
+        # Recording tests
         FreeTappingRecordTest(),  # Use this for unconstrained tapping experiment (without markers).
         InfoPage(
             "You passed the tapping recording test! Congratulations.", time_estimate=3
