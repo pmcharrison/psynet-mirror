0de26527f1962289d76d035436c364b53e29a9a4
Peter Harrison <pmch2@cam.ac.uk>
2022-08-19 16:05:04 +0200

    Bugfix

--- demos/mcmcp/test_imports.py
+++ demos/mcmcp/test_imports.py
@@ -1,12 +1,2 @@
-import os
-
-import pytest
-
-pytest_plugins = ["pytest_dallinger", "psynet.test"]
-
-experiment_dir = os.path.dirname(__file__)
-
-
-@pytest.mark.parametrize("experiment_directory", [experiment_dir], indirect=True)
-def test_experiment(launched_experiment):
-    launched_experiment.test_experiment()
+class CustomCls:
+    pass
