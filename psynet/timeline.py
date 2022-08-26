33eae2786430650e5b007c3b9aeada06d8f6333d
Peter Harrison <pmch2@cam.ac.uk>
2022-08-26 20:24:09 +0200

    Bugfix

--- psynet/timeline.py
+++ psynet/timeline.py
@@ -2488,7 +2488,7 @@ def randomize(label, logic):
     total_time = sum(elt.time_estimate for elt in logic)
     return for_loop(
         label,
-        lst=lambda: random.sample(range(n), n),
+        iterate_over=lambda: random.sample(range(n), n),
         logic=lambda i: logic[i],
         time_estimate_per_iteration=total_time / n,
     )
