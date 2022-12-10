2c86ccb514305cee6440b9815d1fee28bda6d787
Peter Harrison <pmch2@cam.ac.uk>
2022-12-10 06:06:54 +0000

    Tests fix

--- psynet/pytest_psynet.py
+++ psynet/pytest_psynet.py
@@ -293,7 +293,7 @@ def debug_experiment(request, env, clear_workers, in_experiment_directory, db_se
 
     p = pexpect.spawn(
         "psynet",
-        ["debug", "--no-browsers", "--verbose", "--legacy"],
+        ["local", "debug", "--no-browsers", "--verbose", "--legacy"],
         env=env,
         encoding="utf-8",
     )
