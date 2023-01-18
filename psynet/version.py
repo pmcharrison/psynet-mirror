665bbaa5aac029c327bfe69bb7ad7b0963fd98b0
Frank Höger <frank.hoeger@ae.mpg.de>
2023-01-18 04:24:57 +0100

    Increase dallinger_minimum_version to 9.3.1

--- psynet/version.py
+++ psynet/version.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Bump versions by changing these two lines. setup.py will read them automatically.
-dallinger_minimum_version = "9.3.0"
+dallinger_minimum_version = "9.3.1"
 psynet_version = "10.0.0rc0"
 
 dallinger_minimum_version_parts = dallinger_minimum_version.split(".")
