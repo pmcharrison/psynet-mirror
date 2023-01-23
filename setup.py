3b14f8f2d70e474076def26ce0b1afb07875d225
Frank Höger <frank.hoeger@ae.mpg.de>
2023-01-23 16:10:10 +0100

    Update dallinger to version >=9.3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     include_package_data=True,
     package_data={"psynet": ["VERSION"]},
     install_requires=[
-        "dallinger>=9.3.0, <10.0.0",
+        "dallinger>=9.3.1, <10.0.0",
         "click",
         "datetime",
         "dominate",
