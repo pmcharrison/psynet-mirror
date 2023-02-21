695ae3d6484a74ba84b92c6351c263e196b7bc19
Pol van Rijn <rijn@posteo.de>
2023-02-21 13:34:56 +0100

    It should be 12 active bot trails, test passes locally.

--- demos/headphone_test/experiment.py
+++ demos/headphone_test/experiment.py
@@ -40,4 +40,4 @@ class Exp(psynet.experiment.Experiment):
     )
 
     def test_check_bot(self, bot: Bot, **kwargs):
-        assert len(bot.alive_trials) == 6
+        assert len(bot.alive_trials) == 12
