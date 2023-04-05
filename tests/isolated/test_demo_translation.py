60457a638b1230130e3dfb501a3e8280beb4015a
Pol van Rijn <rijn@posteo.de>
2023-04-05 21:08:33 +0200

    Updated tests

--- tests/isolated/test_demo_translation.py
+++ tests/isolated/test_demo_translation.py
@@ -5,6 +5,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
+from psynet.experiment import get_and_load_config
 from psynet.pytest_psynet import assert_text, bot_class, next_page, path_to_demo
 
 logger = logging.getLogger(__file__)
@@ -18,11 +19,9 @@ EXPERIMENT = None
 @pytest.mark.usefixtures("launched_experiment")
 class TestExp(object):
     def test_variables(self, db_session):
-        from psynet.experiment import get_experiment
-
-        exp = get_experiment()
-        assert exp.var.supported_locales == ["en", "de", "nl"]
-        assert exp.var.allow_switching_locale is True
+        configuration = get_and_load_config()
+        assert configuration.get("supported_locales") == '["en", "de", "nl"]'
+        assert configuration.get("allow_switching_locale") is True
 
     def test_exp(self, bot_recruits, db_session):  # two_iterations, bot_recruits):
         for i, bot in enumerate(bot_recruits):
