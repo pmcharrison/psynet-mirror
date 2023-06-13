0df135ea7becf6eac2953e31dcc7112731b551ff
Frank Höger <frank.hoeger@ae.mpg.de>
2023-06-13 08:55:40 +0200

    Cleanup log message

--- psynet/lucid.py
+++ psynet/lucid.py
@@ -308,10 +308,6 @@ class LucidService(object):
                 termination_time_in_s
                 - (datetime.now() - lucid_rid.creation_time).seconds
             )
-            if time_until_termination_in_s % 10 == 0:
-                logger.info(
-                    f"Seconds until termination of RID '{rid}': {time_until_termination_in_s}"
-                )
             return time_until_termination_in_s
 
     def send_complete_request(self, rid):
