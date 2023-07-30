d86654892f902d37918c29e832c939017b0eaa9d
Frank Höger <frank.hoeger@ae.mpg.de>
2023-07-31 00:03:33 +0200

    Bump version to 10.2.0

--- psynet/version.py
+++ psynet/version.py
@@ -3,7 +3,7 @@ import re
 
 # Bump versions by changing these two lines. setup.py will read them automatically.
 dallinger_minimum_version = "9.9.0"
-psynet_version = "10.1.0"
+psynet_version = "10.2.0"
 
 dallinger_minimum_version_parts = dallinger_minimum_version.split(".")
 dallinger_maximum_major_version = int(dallinger_minimum_version_parts[0]) + 1
