3f999d71fad0c430e5e4bc6ca79312a479ef8d27
Frank Höger <frank.hoeger@ae.mpg.de>
2023-09-19 19:12:00 +0000

    Throw an error when requirements.txt specifies a different version of PsyNet to what is locally installed

--- psynet/version.py
+++ psynet/version.py
@@ -1,5 +1,9 @@
 import os
 import re
+import subprocess
+
+from dallinger.version import __version__ as dallinger_version
+from yaspin import yaspin
 
 # Bump versions by changing these two lines. setup.py will read them automatically.
 dallinger_minimum_version = "9.10.0"
@@ -11,6 +15,131 @@ dallinger_maximum_version = f"{dallinger_maximum_major_version}.0.0"
 dallinger_version_requirement = (
     f"dallinger>={dallinger_minimum_version}, <{dallinger_maximum_version}"
 )
+
+
+def check_versions():
+    "Check whether the PsyNet and Dallinger versions installed locally match the ones specified in the requirements.txt file."
+    if os.environ.get("SKIP_VERSION_CHECK"):
+        print(
+            "SKIP_VERSION_CHECK is set so we will skip checking PsyNet versions specified vs. installed."
+        )
+        return
+
+    with yaspin(
+        text="Checking whether PsyNet and Dallinger versions specified and installed are the same...",
+        color="green",
+    ) as spinner:
+        with open("requirements.txt", "r") as file:
+            versions = get_all_version_infos(file.read())
+
+            for package_name, version_infos in versions.items():
+                consistent = version_infos["consistent"]
+
+                if consistent:
+                    spinner.ok("✔")
+                else:
+                    spinner.color = "red"
+                    spinner.fail("✗")
+
+                    assert consistent, (
+                        f"The {package_name} versions installed on your local computer and specified in requirements.txt do not match.\n"
+                        f'\nVersion installed locally: {version_infos["installed"]}'
+                        f'\nVersion specified in requirements.txt: {version_infos["specified"]}'
+                        "\n\nYou can skip this check by writing `export SKIP_VERSION_CHECK=1` (without quotes) in your terminal."
+                    )
+
+
+def get_all_version_infos(file_content):
+    versions = {}
+    for package_name in ["Dallinger", "PsyNet"]:
+        versions[package_name] = {
+            "specified": None,
+            "installed": None,
+            "consistent": True,
+        }
+        specified = None
+        installed = None
+
+        requirements = [
+            line for line in file_content.splitlines() if package_name.lower() in line
+        ]
+
+        if len(requirements) == 0:
+            continue
+
+        requirement = requirements[0]
+
+        match = re.search(
+            f"/{package_name}@(.*)#egg={package_name}", requirement, re.IGNORECASE
+        )
+        if match is None:
+            match = re.search(f"/{package_name}@(.*)", requirement, re.IGNORECASE)
+
+        # We either assume PsyNet is specified in the correct requirement syntax
+        # or as a standard requirement (e.g. 'psynet==10.0.0')
+        specified = (
+            match.group(1) if match is not None else re.split("==", requirement)[-1]
+        )
+
+        # In case just specified as the package name
+        if specified == package_name.lower():
+            continue
+
+        if specified.startswith("v"):
+            # Get installed version via the Dallinger/PsyNet API
+            installed = installed_version_for(package_name)
+            specified = specified[1:]
+
+        if re.search("^([0-9]+)\\.([0-9]+)\\.([0-9]+)$", specified) is not None:
+            # Get installed version via the Dallinger/PsyNet API
+            installed = installed_version_for(package_name)
+        else:
+            # Get installed version from `pip freeze`
+            installed = commit_hash_or_version_from_pip_freeze(package_name)
+
+        consistent = specified is None or specified == installed
+
+        versions[package_name] = {
+            "specified": specified,
+            "installed": installed,
+            "consistent": consistent,
+        }
+
+    return versions
+
+
+def installed_version_for(package_name):
+    import psynet
+
+    if package_name == "Dallinger":
+        return dallinger_version
+    if package_name == "PsyNet":
+        return psynet.__version__
+    raise ValueError(f"Unsupported package '{package_name}'")
+
+
+def get_pip_freeze_requirement(name):
+    pip_freeze_stdout = subprocess.run(
+        ["pip freeze"],
+        shell=True,
+        capture_output=True,
+    ).stdout
+
+    return [
+        line.decode()
+        for line in pip_freeze_stdout.splitlines()
+        if f"{name}" in line.decode()
+    ][0]
+
+
+def commit_hash_or_version_from_pip_freeze(package_name):
+    line = get_pip_freeze_requirement(package_name)
+    match = re.search(f"{package_name}@(.*)#egg={package_name}", line, re.IGNORECASE)
+    if match is None:
+        match = re.search(f"{package_name}@(.*)", line, re.IGNORECASE)
+    if match is not None:
+        return match.group(1)
+    return line.split("==")[-1]
 
 
 def parse_version(x):
