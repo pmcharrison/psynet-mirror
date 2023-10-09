0d7b6fd33dc47591c391b189f15525076c59331b
Frank Höger <frank.hoeger@ae.mpg.de>
2023-10-09 19:40:02 +0200

    Un-add 'Participant.reward' method

--- psynet/participant.py
+++ psynet/participant.py
@@ -195,10 +195,6 @@ class Participant(SQLMixinDallinger, dallinger.models.Participant):
     #     self.module_state.current_trial = value
 
     @property
-    def reward(self):
-        return self.bonus + self.base_payment
-
-    @property
     def last_response(self):
         from psynet.timeline import Response
 
