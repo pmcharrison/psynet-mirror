7d09b3bcda84deb58fb9694bd9be840e2106ff25
Frank Höger <frank.hoeger@ae.mpg.de>
2023-12-04 17:53:16 +0100

    Move test to consent_rejected demo

--- demos/timeline/experiment.py
+++ demos/timeline/experiment.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-from typing import List
 
 import numpy
-import requests
 from dallinger.experiment import experiment_route
 from dominate import tags
 
 import psynet.experiment
-from psynet.bot import Bot
 from psynet.consent import MainConsent
 from psynet.modular_page import (
     ModularPage,
@@ -18,7 +15,6 @@ from psynet.modular_page import (
     TimedPushButtonControl,
 )
 from psynet.page import InfoPage, SuccessfulEndPage
-from psynet.recruiters import DevLucidRecruiter, MTurkRecruiter, ProlificRecruiter
 from psynet.timeline import (
     CodeBlock,
     Module,
@@ -39,12 +35,8 @@ class Exp(psynet.experiment.Experiment):
     }
 
     config = {
-        "lucid_api_key": "secret",
-        "lucid_sha1_hashing_key": "secret",
-        "lucid_recruitment_config": "file:./lucid_recruitment_config.json",
         "min_accumulated_reward_for_abort": 0.15,
         "show_abort_button": True,
-        "show_reward": False,
     }
 
     @experiment_route("/custom_route", methods=["POST", "GET"])
@@ -226,62 +218,3 @@ class Exp(psynet.experiment.Experiment):
         ),
         SuccessfulEndPage(),
     )
-
-    def with_recruiter(self, nickname):
-        # We use this for patching the recruiter while testing the recruiter UI
-        if self.var.has("with_recruiter"):
-            patched_recruiter = self.var.with_recruiter
-            if nickname == "mturk":
-                self.recruiter = MTurkRecruiter(skip_config_validation=True)
-            elif nickname == "prolific":
-                self.recruiter = ProlificRecruiter()
-            elif nickname == "lucid":
-                self.recruiter = DevLucidRecruiter()
-            return nickname == patched_recruiter
-        return super().with_recruiter(nickname)
-
-    def take_page_and_make_request(self, bot):
-        bot.take_page(bot.get_current_page(), response={"main_consent": False})
-        return requests.get(f"http://localhost:5000/timeline?unique_id={bot.unique_id}")
-
-    test_n_bots = 4
-
-    def run_bot(self, bot):
-        # Hotair
-        if bot.id == 1:
-            req = self.take_page_and_make_request(bot)
-            assert "Consent was rejected." in str(req.content)
-            assert "End of experiment." in str(req.content)
-            assert 'Please click "Finish" to complete the experiment.' in str(
-                req.content
-            )
-            bot.run_to_completion()
-
-        # Lucid
-        if bot.id == 2:
-            self.var.with_recruiter = "lucid"
-            req = self.take_page_and_make_request(bot)
-            assert "Consent was rejected." not in str(req.content)
-            assert "End of experiment." not in str(req.content)
-            bot.run_to_completion()
-
-        # MTurk
-        if bot.id == 3:
-            self.var.with_recruiter = "mturk"
-            req = self.take_page_and_make_request(bot)
-            assert "Consent was rejected." in str(req.content)
-            assert "End of experiment." in str(req.content)
-            assert 'Please click "Finish" to complete the HIT.' in str(req.content)
-            bot.run_to_completion()
-
-        # Prolific
-        if bot.id == 4:
-            self.var.with_recruiter = "prolific"
-            req = self.take_page_and_make_request(bot)
-            assert "Consent was rejected." in str(req.content)
-            assert "End of experiment." in str(req.content)
-            assert 'Please click "Finish" to complete the study.' in str(req.content)
-            bot.run_to_completion()
-
-    def test_check_bots(self, bots: List[Bot]):
-        super().test_check_bots(bots, failed=True)
