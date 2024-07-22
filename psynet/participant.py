d63cafa49e12efc4a1533f299437d12d81214e7b
Peter Harrison <pmch2@cam.ac.uk>
2024-07-23 00:36:43 +0200

    Minor update to start_module

--- psynet/participant.py
+++ psynet/participant.py
@@ -403,6 +403,7 @@ class Participant(SQLMixinDallinger, dallinger.models.Participant):
         state = module.state_class(module, self)
         state.start()
         self.module_state = state
+        self._module_states.append(state)
 
     def end_module(self, module):
         # This should only fail (delivering multiple logs) if the experimenter has perversely
