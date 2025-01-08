9d96a142af7003758f1fcc101b7f77b96684d5ff
Pol van Rijn <pol.van-rijn@ae.mpg.de>
2025-01-08 10:56:08 +0100

    Implemented translators no testing

--- psynet/translation/translators.py
+++ psynet/translation/translators.py
@@ -1,122 +1,156 @@
-import warnings
-from functools import cached_property
 from typing import List
 
-import requests
+from psynet.utils import get_config, get_descendent_class_by_name, get_language_dict
 
 
 class Translator:
-    def translate(self, texts: List[str], source_lang: str, target_lang: str):
+    nickname = None
+
+    def translate(
+        self,
+        texts: List[str],
+        source_lang: str,
+        target_lang: str,
+        file_path: str = None,
+    ):
         raise NotImplementedError
 
-    def _get_response(self, url, json_data):
-        response = requests.post(url, json=json_data)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception(f"Error: {response.status_code} {response.reason}")
+
+class CredentialsError(Exception):
+    pass
 
 
-class DeepLTranslator(Translator):
-    # TODO - Enable retry logic once we know what kind of exception needs catching
-    # @retry(
-    #     retry=retry_if_exception_type(NoResultFound),
-    #     wait=wait_exponential(multiplier=1, min=3),
-    #     stop=stop_after_delay(4)
-    # )
-    def translate(self, texts: List[str], source_lang: str, target_lang: str):
-        assert isinstance(texts, list)
-        payload = self._make_payload(texts, source_lang, target_lang)
-        response = self._get_response("https://api.deepl.com/jsonrpc", payload)
-        return self._parse_response(response)
-
-    def _make_payload(self, sentences: List[str], source_lang: str, target_lang: str):
-        return {
-            "jsonrpc": "2.0",
-            "method": "LMT_handle_jobs",
-            "params": {
-                "tag_handling": "xml",
-                "jobs": self._create_jobs(sentences),
-                "lang": {
-                    "source_lang_computed": source_lang.upper(),
-                    "target_lang": target_lang.upper(),
-                },
-                "priority": 1,
-                "commonJobParams": {
-                    "mode": "translate",
-                    "browserType": 1,
-                    "formality": "formal",
-                },
-            },
-        }
-
-    def _create_jobs(self, sentences, num_alternatives=1):
-        return [
-            {
-                "kind": "default",
-                "sentences": [
-                    {
-                        "text": sentence,
-                        "id": i,
-                        "prefix": "",
-                    },
-                ],
-                "raw_en_context_before": sentences[:i],
-                "raw_en_context_after": sentences[i + 1 :],
-                "preferred_num_beams": num_alternatives,
-            }
-            for i, sentence in enumerate(sentences)
-        ]
-
-    def _parse_response(self, json_response):
-        return [
-            translation["beams"][0]["sentences"][0]["text"]
-            for translation in json_response["result"]["translations"]
-        ]
-
-
-class MetaTranslator(Translator):
-    def translate(self, texts: List[str], source_lang: str, target_lang: str):
-        from psynet.translation.languages import (
-            get_supported_deepl_languages,
-            get_supported_gtrans_languages,
-        )
-
-        if target_lang in get_supported_deepl_languages():
-            DeepLTranslator().translate(texts, target_lang, source_lang)
-        elif target_lang in get_supported_gtrans_languages():
-            GoogleTranslator().translate(texts, target_lang, source_lang)
-        else:
-            raise NotImplementedError(
-                f"Language {target_lang} is not supported by DeepL and Google Translate. "
-                f"DeepL supports {get_supported_deepl_languages()} and Google Translate supports {get_supported_gtrans_languages()}."
-            )
+class UnsupportedLanguageError(Exception):
+    pass
 
 
 class GoogleTranslator(Translator):
-    @cached_property
-    def _translator(self):
-        from googletrans import Translator
+    nickname = "google_translate"
 
-        return Translator()
+    def translate(
+        self,
+        texts: List[str],
+        source_lang: str,
+        target_lang: str,
+        file_path: str = None,
+    ):
+        from google.cloud import translate_v3
 
-    # TODO - Enable retry logic once we know what kind of exception needs catching
-    # @retry(
-    #     retry=retry_if_exception_type(NoResultFound),
-    #     wait=wait_exponential(multiplier=1, min=3),
-    #     stop=stop_after_delay(4)
-    # )
-    def translate(self, texts: List[str], source_lang: str, target_lang: str):
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=DeprecationWarning)
+        config = get_config()
+        args = {
+            "google_translate_project_id": config.get(
+                "google_translate_project_id", None
+            ),
+            "google_translate_json_path": config.get(
+                "google_translate_json_path", None
+            ),
+        }
+        if not all(args.values()):
+            error_msg = "Please provide the following credentials in your .dallingerconfig file: "
+            for key, value in args.items():
+                if not value:
+                    error_msg += f"{key}, "
+            raise CredentialsError(error_msg)
 
-            assert isinstance(texts, list)
-
-            from googletrans import Translator
-
-            translator = Translator()
-            response = translator.translate(
-                texts, dest=target_lang.lower(), src=source_lang.lower()
+        client = translate_v3.TranslationServiceClient.from_service_account_json(
+            args["google_translate_json_path"]
+        )
+        parent = f"projects/{args['google_translate_project_id']}/locations/global"
+        try:
+            response = client.translate_text(
+                contents=texts,
+                target_language_code=target_lang,
+                parent=parent,
+                mime_type="text/html",
+                source_language_code=source_lang,
             )
+        except Exception as e:
+            if e.args[0] == "Target language is invalid.":
+                raise UnsupportedLanguageError(f"Invalid language code: {target_lang}")
+            else:
+                raise e
 
-            return [translation.text for translation in response]
+        # Display the translation for each input text provided
+        return [translation.translated_text for translation in response.translations]
+
+
+class ChatGptTranslator(Translator):
+    nickname = "chat_gpt"
+
+    def get_system_prompt(
+        self,
+        texts: List[str],
+        source_language: str,
+        target_language: str,
+        file_path: str = None,
+    ):
+        return (
+            f"You are a helpful assistant that translates {source_language} to {target_language}."
+            + """
+                If you see any HTML tags in the text, you should not translate them.
+                If you see any variables in the text, you should not translate them.
+                Variables are written in capital letters and are either surrounded by curly brackets (e.g., {VARIABLE}) or start with "%(" and end with ")s" (e.g., "%(VARIABLE)s").
+                You do not have to keep the original word order.
+                """
+        )
+
+    def translate(
+        self,
+        texts: List[str],
+        source_lang: str,
+        target_lang: str,
+        file_path: str = None,
+    ):
+        from openai import OpenAI
+
+        language_dict = get_language_dict("en")
+        assert (
+            source_lang in language_dict
+        ), f"Source language {source_lang} not found in known languages"
+        source_language = language_dict[source_lang]
+        assert (
+            target_lang in language_dict
+        ), f"Target language {target_lang} not found in known languages"
+        target_language = language_dict[target_lang]
+
+        config = get_config()
+        openai_api_key = config.get("openai_api_key", None)
+        if openai_api_key is None:
+            raise CredentialsError(
+                "Please provide an OpenAI API key in your .dallingerconfig file under `openai_api_key`"
+            )
+        openai_default_temperature = float(config.get("openai_default_temperature"))
+        openai_default_model = config.get("openai_default_model")
+
+        client = OpenAI(api_key=openai_api_key)
+        messages = [
+            # TODO add system prompt with the full file as context
+            {
+                "role": "system",
+                "content": self.get_system_prompt(
+                    texts, source_language, target_language
+                ),
+            },
+        ]
+        for text in texts:
+            messages.append({"role": "user", "content": text})
+        response = client.chat.completions.create(
+            model=openai_default_model,
+            messages=messages,
+            temperature=openai_default_temperature,
+        )
+        return [choice.message.content for choice in response.choices]
+
+
+class DefaultTranslator(Translator):
+    def translate(
+        self,
+        texts: List[str],
+        source_lang: str,
+        target_lang: str,
+        file_path: str = None,
+    ):
+        config = get_config()
+        default_translator = config.get("default_translator")
+        translator_class = get_descendent_class_by_name(Translator, default_translator)
+        return translator_class().translate(texts, source_lang, target_lang, file_path)
