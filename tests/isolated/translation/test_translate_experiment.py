52f770f0e85c28e7c448ca7a085525e6eb8b9e19
Peter Harrison <pmch2@cam.ac.uk>
2025-01-08 09:33:51 +0000

    Update test translate experiment

--- tests/isolated/translation/test_translate_experiment.py
+++ tests/isolated/translation/test_translate_experiment.py
@@ -1,3 +1,6 @@
+import os
+
+import polib
 import pytest
 
 from psynet.pytest_psynet import path_to_test_experiment
@@ -74,6 +77,11 @@ def test_translate_experiment(mocker):
         "psynet.translation.translation.MetaTranslator.translate"
     )
 
+    def mock_translate_func(texts, source_lang, target_lang):
+        return [f"{source_lang} -> {target_lang} {i}" for i in range(len(texts))]
+
+    mock_translate.side_effect = mock_translate_func
+
     translate_experiment(["fr"])
 
     # We expect all texts within experiment.py to be batched into a single call to the translator
@@ -98,3 +106,43 @@ def test_translate_experiment(mocker):
         source_lang="en",
         target_lang="fr",
     )
+
+    # Expect the translation to be written to the PO file
+    po_path = os.path.join("locales", "fr", "LC_MESSAGES", "experiment.po")
+    assert os.path.exists(po_path)
+    po = polib.pofile(po_path)
+
+    # Expected message IDs and their corresponding translations
+    expected_entries = [
+        ("Hello, welcome to my experiment!", "en -> fr 0"),
+        # Unlike the translator, the po file will only contain one entry for "What is your name?".
+        # This is because all entries with the same msgid are merged into a single entry in the PO file.
+        # Only the last translation is kept; en -> fr 1 is therefore omitted from the PO file.
+        ("What is your name?", "en -> fr 2"),
+        ("Hello, {NAME}!", "en -> fr 3"),
+        ("What is your favorite pet?", "en -> fr 4"),
+        ("dog", "en -> fr 5"),
+        ("cat", "en -> fr 6"),
+        ("fish", "en -> fr 7"),
+        ("hamster", "en -> fr 8"),
+        ("bird", "en -> fr 9"),
+        ("snake", "en -> fr 10"),
+        ("Great, I like {PET} too!", "en -> fr 11"),
+    ]
+
+    # Check each entry matches expected msgid and translation
+    for i, (expected_msgid, expected_msgstr) in enumerate(expected_entries):
+        assert po[i].msgid == expected_msgid
+        assert po[i].msgstr == expected_msgstr
+        assert po[i].fuzzy
+
+        # occurrences is a list of tuples, where each tuple contains (filename, line_number)
+        # line_number should be None since we clean the PO file to remove line numbers
+        occurrences = po[i].occurrences
+        assert len(occurrences) == 1
+
+        occurrence = occurrences[0]
+        filename, line_number = occurrence
+
+        assert filename == "experiment.py"
+        assert line_number == "" or line_number is None
