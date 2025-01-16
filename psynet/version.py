713bb8504ebfab683dc1a8a6d3ff8afed1c556ff
Frank Höger <frank.hoeger@ae.mpg.de>
2025-01-16 03:42:03 +0100

    Bump version to 11.9.0

--- psynet/version.py
+++ psynet/version.py
@@ -7,7 +7,7 @@ from yaspin import yaspin
 
 # Bump versions by changing these two lines.
 dallinger_minimum_version = "11.0.0"
-psynet_version = "11.9.0-rc0"
+psynet_version = "11.9.0"
 
 dallinger_minimum_version_parts = dallinger_minimum_version.split(".")
 dallinger_maximum_major_version = int(dallinger_minimum_version_parts[0]) + 1
