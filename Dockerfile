a4f55f7e4b98614cf49c7cd24d8626b33bdc5ed6
Frank Höger <frank.hoeger@ae.mpg.de>
2025-02-12 10:20:05 +0100

    Update dallinger version to 11.1.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/dallinger/dallinger:11.0.1
+FROM ghcr.io/dallinger/dallinger:11.1.0
 # If you want to pin a Dallinger development version, don't do it here!
 # Instead pin it below (see comments)
 #
--- psynet/version.py
+++ psynet/version.py
@@ -8,7 +8,7 @@ from yaspin import yaspin
 psynet_version = "11.10.0-dev0"
 
 # Bump Dallinger version by changing the line below
-dallinger_recommended_version = "11.0.1"
+dallinger_recommended_version = "11.1.0"
 
 
 def check_versions():
