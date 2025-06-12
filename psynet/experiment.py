67a09d358ed8a86880833e4ecd7cf74db32398fd
Frank Höger <frank.hoeger@ae.mpg.de>
2025-06-12 03:16:19 +0200

    Set check_dallinger_version = False

--- psynet/experiment.py
+++ psynet/experiment.py
@@ -1260,7 +1260,7 @@ class Experiment(dallinger.experiment.Experiment, metaclass=ExperimentMeta):
             "allow_mobile_devices": False,
             "base_payment": 0.10,
             "big_base_payment": False,
-            "check_dallinger_version": True,
+            "check_dallinger_version": False,
             "clock_on": True,
             "color_mode": "light",
             "currency": "$",
