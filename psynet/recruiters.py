c1b278a633dca4a077b0fba48caad02cb079d688
Peter Harrison <pmch2@cam.ac.uk>
2025-07-10 12:58:46 +0100

    Temporary test to see if the AsyncCodeBlock is causing the race condition

--- psynet/recruiters.py
+++ psynet/recruiters.py
@@ -27,8 +27,7 @@ from .data import SQLBase, SQLMixin, register_table
 from .lucid import get_lucid_service
 from .page import InfoPage, WaitPage, wait_while
 from .participant import Participant
-from .timeline import (
-    AsyncCodeBlock,
+from .timeline import (  # AsyncCodeBlock,
     CodeBlock,
     PageMaker,
     Response,
@@ -206,12 +205,14 @@ class PsyNetProlificRecruiterMixin(PsyNetRecruiterMixin):
         )
 
         return join(
-            AsyncCodeBlock(
-                _screen_out_participant,
-                wait=True,
-                expected_wait=5.0,
-                check_interval=0.5,
-            ),
+            # AsyncCodeBlock(
+            #     _screen_out_participant,
+            #     wait=True,
+            #     expected_wait=5.0,
+            #     check_interval=0.5,
+            # ),
+            # Temporary test to see if the AsyncCodeBlock is causing the race condition
+            CodeBlock(_screen_out_participant),
             conditional(
                 # This function should check whether the screen out attempt was successful.
                 label="screen_out_successful",
