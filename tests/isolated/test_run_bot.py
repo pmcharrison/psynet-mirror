cc759fb0cb01659709fdcc3875f95da46564ea24
Peter Harrison <pmch2@cam.ac.uk>
2025-07-29 17:11:04 +0100

    Update bot test for latest participant driver and API changes

--- tests/isolated/test_run_bot.py
+++ tests/isolated/test_run_bot.py
@@ -1,5 +1,3 @@
-import tempfile
-
 import pytest
 
 from psynet.asset import Asset
@@ -18,82 +16,81 @@ class TestRunBot:
     def test_run_bot(self):
         bot = BotDriver()
 
-        with tempfile.TemporaryDirectory() as bot_tempdir:
-            # The first page is a simple multiple choice question,
-            # and does not have any files to upload.
+        # The first page is a simple multiple choice question,
+        # and does not have any files to upload.
 
-            # Check the _render_page function
-            assert Request.query.filter_by(endpoint="/timeline").count() == 0
-            bot._render_page()
-            assert Request.query.filter_by(endpoint="/timeline").count() == 1
+        # Check the _render_page function
+        # We start with one request already, because the bot driver
+        # makes a request to the /timeline endpoint when it is initialized.
+        assert Request.query.filter_by(endpoint="/timeline").count() == 1
+        bot._render_page()
+        assert Request.query.filter_by(endpoint="/timeline").count() == 2
 
-            # Check the _fetch_status function
-            status, response_files = bot._fetch_status(bot_tempdir)
-            assert status["status"] == "working"
-            assert status["page"]["id"] == [0]
-            assert status["page"]["label"] == "favourite_colour"
-            assert status["page"]["time_estimate"] == 5
-            assert status["page"]["bot_response"]["answer"] == "red"
-            assert len(response_files) == 0
+        # Check the _fetch_status function
+        bot._fetch_status()
+        status = bot.status
+        response_files = bot.response_files
+        assert status["status"] == "working"
+        assert status["page"]["id"] == [0]
+        assert status["page"]["label"] == "favourite_colour"
+        assert status["page"]["time_estimate"] == 5
+        assert status["page"]["bot_response"]["raw_answer"] == "red"
+        assert len(response_files) == 0
 
-            # Check the _submit_response function
-            bot._submit_response(status, response_files)
-            assert Response.query.count() == 1
+        # Check the _submit_response function
+        bot._submit_response(status, response_files)
+        assert Response.query.count() == 1
 
-        with tempfile.TemporaryDirectory() as bot_tempdir:
-            # The second page is an audio recording page,
-            # so the bot response involves uploading a file.
+        # The second page is an audio recording page,
+        # so the bot response involves uploading a file.
 
-            # Check the _render_page function
-            assert Request.query.filter_by(endpoint="/timeline").count() == 1
-            bot._render_page()
-            assert Request.query.filter_by(endpoint="/timeline").count() == 2
+        # Check the _render_page function
+        assert Request.query.filter_by(endpoint="/timeline").count() == 2
+        bot._render_page()
+        assert Request.query.filter_by(endpoint="/timeline").count() == 3
 
-            # Check the _fetch_status function
-            status, response_files = bot._fetch_status(bot_tempdir)
-            assert status["page"]["id"] == [1]
-            assert status["page"]["label"] == "record_audio"
-            assert status["page"]["time_estimate"] == 5
+        # Check the _fetch_status function
+        bot._fetch_status()
+        status = bot.status
+        response_files = bot.response_files
+        assert status["page"]["id"] == [1]
+        assert status["page"]["label"] == "record_audio"
+        assert status["page"]["time_estimate"] == 5
 
-            assert len(response_files) == 1
-            file_name = response_files["audioRecording"]
-            with open(file_name, "r") as f:
-                assert f.read() == f"This is a recording from {bot.participant_id}!"
+        assert len(response_files) == 1
+        file_name = response_files["audioRecording"]
+        with open(file_name, "r") as f:
+            assert f.read() == f"This is a recording from {bot.id}!"
 
-            # Check the _submit_response function
-            assert Asset.query.count() == 0
-            bot._submit_response(status, response_files)
-            assert Response.query.count() == 2
-            assert Asset.query.count() == 1
+        # Check the _submit_response function
+        assert Asset.query.count() == 0
+        bot._submit_response(status, response_files)
+        assert Response.query.count() == 2
+        assert Asset.query.count() == 1
 
         # Third page: video recording (camera and screen)
-        with tempfile.TemporaryDirectory() as bot_tempdir:
-            assert Request.query.filter_by(endpoint="/timeline").count() == 2
-            bot._render_page()
-            assert Request.query.filter_by(endpoint="/timeline").count() == 3
+        assert Request.query.filter_by(endpoint="/timeline").count() == 3
+        bot._render_page()
+        assert Request.query.filter_by(endpoint="/timeline").count() == 4
 
-            status, response_files = bot._fetch_status(bot_tempdir)
-            assert status["page"]["id"] == [2]
-            assert status["page"]["label"] == "record_video"
-            assert status["page"]["time_estimate"] == 5
+        bot._fetch_status()
+        status = bot.status
+        response_files = bot.response_files
+        assert status["page"]["id"] == [2]
+        assert status["page"]["label"] == "record_video"
+        assert status["page"]["time_estimate"] == 5
 
-            assert set(response_files.keys()) == {"cameraRecording", "screenRecording"}
-            with open(response_files["cameraRecording"], "r") as f:
-                assert (
-                    f.read()
-                    == f"This is a camera recording from bot {bot.participant_id}."
-                )
-            with open(response_files["screenRecording"], "r") as f:
-                assert (
-                    f.read()
-                    == f"This is a screen recording from bot {bot.participant_id}."
-                )
+        assert set(response_files.keys()) == {"cameraRecording", "screenRecording"}
+        with open(response_files["cameraRecording"], "r") as f:
+            assert f.read() == f"This is a camera recording from bot {bot.id}."
+        with open(response_files["screenRecording"], "r") as f:
+            assert f.read() == f"This is a screen recording from bot {bot.id}."
 
-            prev_response_count = Response.query.count()
-            prev_asset_count = Asset.query.count()
-            bot._submit_response(status, response_files)
-            assert Response.query.count() == prev_response_count + 1
-            assert Asset.query.count() == prev_asset_count + 2
+        prev_response_count = Response.query.count()
+        prev_asset_count = Asset.query.count()
+        bot._submit_response(status, response_files)
+        assert Response.query.count() == prev_response_count + 1
+        assert Asset.query.count() == prev_asset_count + 2
 
     def test_get_current_page(self):
         bot = BotDriver()
