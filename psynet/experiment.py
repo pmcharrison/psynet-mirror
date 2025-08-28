e92e6b0e794d94238737f94009517c34a5d91137
Peter Harrison <pmch2@cam.ac.uk>
2025-08-28 17:46:08 +0100

    Add index_html

--- psynet/experiment.py
+++ psynet/experiment.py
@@ -55,7 +55,7 @@ from dallinger.utils import get_base_url as dallinger_get_base_url
 from dallinger.version import __version__ as dallinger_version
 from dominate import tags
 from flask import g as flask_app_globals
-from flask import jsonify, redirect, render_template, request, send_file
+from flask import jsonify, redirect, render_template, request, send_file, url_for
 from flask_login import login_required
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, func
 from sqlalchemy.orm import joinedload, with_polymorphic
@@ -540,6 +540,34 @@ class Experiment(dallinger.experiment.Experiment, metaclass=ExperimentMeta):
     @cached_property
     def authenticated_session(self):
         return get_authenticated_session(self.base_url)
+
+    @classmethod
+    def get_index_html(cls):
+        return f"""
+<html>
+<head>
+    <title>PsyNet Experiment</title>
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 class="mb-4">PsyNet Experiment</h1>
+            <p class="mb-3">
+                If you are a participant, you are in the wrong place, please double-check your URL!
+            </p>
+            <p>
+                If you are an experimenter, please
+                <a href="{url_for("dashboard.dashboard_index")}">click here</a>
+                to access the dashboard.
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+"""
 
     @classproperty
     def hidden_dashboards(cls):
