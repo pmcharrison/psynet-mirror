4351a6cf7a9e973f422f02b0ebadf4e7ff17e6ef
Peter Harrison <pmch2@cam.ac.uk>
2025-09-02 16:57:13 +0100

    Move test experiment to demos directory

--- demos/features/pre_deploy_constant/experiment.py
+++ demos/features/pre_deploy_constant/experiment.py
@@ -4,23 +4,26 @@ import psynet.experiment
 from psynet.bot import Bot
 from psynet.experiment import pre_deploy_constant
 from psynet.page import InfoPage
-from psynet.timeline import CodeBlock, Timeline
+from psynet.timeline import CodeBlock, PageMaker, Timeline
 
 local_machine_cwd = pre_deploy_constant("local_machine_cwd", lambda: os.getcwd())
-data_files = pre_deploy_constant("data_files", lambda: os.listdir("data"))
+data_files = pre_deploy_constant("data_files", lambda: sorted(os.listdir("data")))
 
 
 class Exp(psynet.experiment.Experiment):
     label = "Pre-deploy constant"
 
     timeline = Timeline(
-        InfoPage(
-            """
-            We need at least one page at the start of the timeline, otherwise the code blocks will be evaluated immediately
-            on initialization of the testing script, and this will result in the code being run in the test directory
-            rather than the deployment directory.
-            """,
-            time_estimate=5,
+        PageMaker(
+            lambda: InfoPage(
+                f"""
+Pre-deploy constants are used to store values that can only be computed on the local machine,
+not on the deployed machine. For example, the 'data' directory is only available on the local machine,
+so we need to use a pre-deploy constant to store the list of files in the 'data' directory.
+In this case, `data_files` has taken the following value: `{data_files}`.
+"""
+            ),
+            time_estimate=10,
         ),
         CodeBlock(
             lambda participant: participant.var.set(
