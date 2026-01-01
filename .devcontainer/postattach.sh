73ef34b10838de1b0db51c6598cfdc191792ec96
Peter Harrison <pmch2@cam.ac.uk>
2026-01-01 21:29:50 +0000

    Further updates to devcontainer scripts

--- .devcontainer/postattach.sh
+++ .devcontainer/postattach.sh
@@ -1,11 +1,5 @@
 # This code runs when the user attaches to the development container.
 
-# This code creates a symbolic link from the working directory to the user directory.
-# Users can update values in the visible .dallingerconfig file and they will be automatically loaded by experiments.
-# They must make sure not to delete it, though.
-rm /root/.dallingerconfig || true
-ln -s /workspaces/${RepositoryName}/.devcontainer/.dallingerconfig /root/.dallingerconfig
-
 
 # The following code launches the Redis and Postgres services in Docker containers.
 echo "Preparing services:"
