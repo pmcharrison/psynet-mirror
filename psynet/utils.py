5aab8ce33cc7b90539d0f78d55ed1a06c730103a
project_41336520_bot_5be76cc707025643ffe90a607c99ff3b <project_41336520_bot_5be76cc707025643ffe90a607c99ff3b@noreply.gitlab.com>
2026-01-29 17:20:42 +0000

    Fix linspace edge case
    Co-authored-by: pmch2 <pmch2@cam.ac.uk>

--- psynet/utils.py
+++ psynet/utils.py
@@ -280,6 +280,8 @@ def linspace(lower, upper, length: int):
     length : int
         The length of the resulting list.
     """
+    if length == 1:
+        return [lower]
     return [lower + x * (upper - lower) / (length - 1) for x in range(length)]
 
 
