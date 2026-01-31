ac334dd48339bc465b31352024ca90223e4876e3
project_41336520_bot_5be76cc707025643ffe90a607c99ff3b <project_41336520_bot_5be76cc707025643ffe90a607c99ff3b@noreply.gitlab.com>
2026-01-31 14:40:31 +0000

    Consolidate block sync
    Co-authored-by: pmch2 <pmch2@cam.ac.uk>

--- psynet/trial/chain.py
+++ psynet/trial/chain.py
@@ -1758,18 +1758,12 @@ class ChainTrialMaker(NetworkTrialMaker):
             return "exit"
 
         if self._should_finish_block(participant):
-            if (
-                participant.module_state.block_position + 1
-                >= participant.module_state.n_blocks
-            ):
+            next_block_position = participant.module_state.block_position + 1
+            if next_block_position >= participant.module_state.n_blocks:
                 return "exit"
             else:
-                if self.sync_group_type:
-                    group = participant.active_sync_groups[self.sync_group_type]
-                    for p in group.participants:
-                        p.module_state.go_to_next_block()
-                else:
-                    participant.module_state.go_to_next_block()
+                next_block = participant.module_state.block_order[next_block_position]
+                self._sync_block_state(participant, next_block)
 
         # networks = db.session.query(
         #     self.network_class.chain_type,
