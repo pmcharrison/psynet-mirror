c4cacc5cb6d71cf7c09745b7e42ad44a01f2fc44
project_41336520_bot_5be76cc707025643ffe90a607c99ff3b <project_41336520_bot_5be76cc707025643ffe90a607c99ff3b@noreply.gitlab.com>
2026-02-02 13:21:38 +0000

    Create cue_static assets per iteration
    Co-authored-by: pmch2 <pmch2@cam.ac.uk>

--- tests/experiments/cue_static/experiment.py
+++ tests/experiments/cue_static/experiment.py
@@ -31,7 +31,7 @@ class Exp(psynet.experiment.Experiment):
         for_loop(
             label="loop over custom trials",
             iterate_over=lambda: range(3),
-            logic=CustomTrial.cue(
+            logic=lambda _item, experiment, participant: CustomTrial.cue(
                 definition={},
                 assets={"stimulus": asset("static/stimulus.txt")},
             ),
