4cc6723ffc12cb8403603fbdde6022261051f623
Frank Höger <fmhoeger@mirsem.org>
2026-02-17 14:08:30 -0500

    Bump version to 13.1.0

--- psynet/version.py
+++ psynet/version.py
@@ -6,7 +6,7 @@ import click
 from dallinger.version import __version__ as dallinger_version
 from yaspin import yaspin
 
-psynet_version = "13.1.0rc3"
+psynet_version = "13.1.0"
 
 # Specify Dallinger MAJOR.MINOR version to allow any patch in that series
 dallinger_recommended_version = "12.1"
