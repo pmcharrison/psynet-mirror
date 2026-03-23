4819a1d36b480d419945db6bc709d11e8c3a597d
project_41336520_bot_5be76cc707025643ffe90a607c99ff3b <project_41336520_bot_5be76cc707025643ffe90a607c99ff3b@noreply.gitlab.com>
2026-03-23 21:10:57 +0000

    Add failing export regression test
    Co-authored-by: pmch2 <pmch2@cam.ac.uk>

--- tests/isolated/test_export_datetime_format.py
+++ tests/isolated/test_export_datetime_format.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from psynet.data import _db_instance_to_dict
+from sqlalchemy.ext.mutable import MutableDict
 
 
 class _DummyExportObject:
@@ -13,3 +14,14 @@ def test_db_instance_to_dict_formats_datetime():
 
     assert data["creation_time"] == "2026-02-07 12:34:56"
     assert "py/object" not in data["creation_time"]
+
+
+class _DummyRequestLikeExportObject:
+    def to_dict(self):
+        return {"params": MutableDict({"recruiter": "hotair", "mode": "debug"})}
+
+
+def test_db_instance_to_dict_formats_mutable_dict_as_plain_dict():
+    data = _db_instance_to_dict(_DummyRequestLikeExportObject(), scrub_pii=False)
+
+    assert data["params"] == {"recruiter": "hotair", "mode": "debug"}
