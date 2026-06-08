ddb302b915ea027ba202469e142bce68d81a8400
Peter Harrison <pmch2@cam.ac.uk>
2026-06-08 20:58:41 +0000

    Stabilize S3 storage tests

--- psynet/pytest_psynet.py
+++ psynet/pytest_psynet.py
@@ -43,7 +43,10 @@ from .data import init_db
 from .experiment import get_experiment, import_local_experiment
 from .modular_page import ModularPage, PushButtonControl
 from .redis import redis_vars
-from .test_helpers.mock_s3 import get_artifact_storage_s3_test_client
+from .test_helpers.mock_s3 import (
+    get_mock_s3_client,
+    get_mock_s3_resource,
+)
 from .testing.chrome_driver import create_psynet_chrome_driver
 from .trial.main import TrialNetwork
 from .trial.static import StaticNode, StaticTrial, StaticTrialMaker
@@ -94,8 +97,11 @@ def assert_text(driver, element_id, value):
 @pytest.fixture
 def artifact_storage_s3_test_root(tmp_path, monkeypatch):
     root = str(tmp_path / "psynet-artifact-storage-s3-test")
-    client = get_artifact_storage_s3_test_client(root)
+    client = get_mock_s3_client(root)
+    resource = get_mock_s3_resource(root)
     monkeypatch.setattr(psynet_asset, "get_s3_client", lambda: client)
+    monkeypatch.setattr(psynet_asset, "get_s3_resource", lambda: resource)
+    monkeypatch.setattr(psynet_asset, "get_s3_bucket", resource.Bucket)
     monkeypatch.setattr(psynet_artifact, "get_s3_client", lambda: client)
     psynet_asset.list_files_in_s3_bucket__cached.cache_clear()
     try:
